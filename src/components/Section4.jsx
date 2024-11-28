9c96d366a9df88869d4c3c483b4714d8a5acb701
Joaobatiist <joaovpbo@outlook.com>
2024-11-28 00:25:21 -0300

    wokwi
    Trazendo o produto para dentro do nosso site.

---
+++ src/components/Section4.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+function Section4 (){
+    return (
+    <Box
+      id="product-section"
+      sx={{
+        backgroundColor: "#00000",
+        width: "100%",
+        height: "100vh",
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",  
+        alignItems: "center",      
+      }}
+    >
+    <Box
+        sx={{
+          width: "640px",          
+          height: "640px",         
+          backgroundColor: "#ffffff",  
+          borderRadius: "5px",     
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",  
+          padding: "3px",          
+        }}
+      >
+        <iframe
+          src="https://wokwi.com/projects/411294519932756993"
+          title="Simulador Wokwi"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          allowFullScreen
+        />
+      </Box>
+    </Box>
+  );
+     
+    }
+
+
+
+export default Section4;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import { Typography } from "@mui/material";
 import Section2 from "../components/Section2";
 import Section3 from "../components/Section3";
+import Section4 from "../components/Section4";
 
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
       <Section1 />
       <Section2/>
       <Section3/>
+      <Section4/>
     </div>
   );
 }
